b608490aa47caabc1bde33443430a6763f0cff3c
Sharonoito <oitosharonakoth@gmail.com>
2021-07-22 12:00:53 +0300

    salon

---
+++ main.js
@@ -0,0 +1,27 @@
+// const marker=document.querySelector('#marker');
+// const item=document.querySelectorAll("ul li a");
+
+// function indicator(e){
+//     marker.style.left=e.offsetLeft+'px';
+//     marker.style.width=e.offsetWidth+'px'
+// }
+// item.forEach(link =>{
+//     link.addEventListener('mousemove', (e)=>{
+//         Indicator(e.target);
+
+//     })
+// })
+
+
+// var li = document.getElementsByTagName('li')[0];
+
+// li.onmouseover = function() {
+//   li.firstElementChild.style.color = "#F00"; // red
+// }
+
+// li.onmouseout = function() {
+//   li.firstElementChild.style.color = "#000"; // black
+// };
+// <li>
+//   <a href="#">Mouse over me.</a>
+// </li>
