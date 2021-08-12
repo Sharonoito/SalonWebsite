6d5e16485f541388cb87492b14fb3a5361726ec8
Sharonoito <oitosharonakoth@gmail.com>
2021-08-12 12:57:47 +0300

    update

--- main.js
+++ main.js
@@ -1,27 +1,16 @@
-// const marker=document.querySelector('#marker');
-// const item=document.querySelectorAll("ul li a");
+let modalBtn = document.getElementById("modal-btn")
+let modal = document.querySelector(".modal")
+let closeBtn = document.querySelector(".close-btn")
+modalBtn.onclick = function(){
+  modal.style.display = "block"
+}
+closeBtn.onclick = function(){
+  modal.style.display = "none"
+}
+window.onclick = function(e){
+  if(e.target == modal){
+    modal.style.display = "none"
+  }
+}
 
-// function indicator(e){
-//     marker.style.left=e.offsetLeft+'px';
-//     marker.style.width=e.offsetWidth+'px'
-// }
-// item.forEach(link =>{
-//     link.addEventListener('mousemove', (e)=>{
-//         Indicator(e.target);
 
-//     })
-// })
-
-
-// var li = document.getElementsByTagName('li')[0];
-
-// li.onmouseover = function() {
-//   li.firstElementChild.style.color = "#F00"; // red
-// }
-
-// li.onmouseout = function() {
-//   li.firstElementChild.style.color = "#000"; // black
-// };
-// <li>
-//   <a href="#">Mouse over me.</a>
-// </li>
